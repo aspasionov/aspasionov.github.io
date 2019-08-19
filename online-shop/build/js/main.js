01ba6182739739b2f37c7237364803dc76cadb34
spasia3run <54273077+spasia3run@users.noreply.github.com>
2019-08-19 16:36:59 +0300

    shop commit

---
+++ online-shop/build/js/main.js
@@ -0,0 +1,15 @@
+
+
+$(document).ready(function(){
+  $('.banner-slider').slick({
+  
+  });
+});
+
+$(document).ready(function(){
+  $('.aside-slider').slick({
+    dots: true,
+    nav: false
+  });
+});
+
