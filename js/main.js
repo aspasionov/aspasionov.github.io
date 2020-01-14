05a1f86218ba08000cad2afba53e090ac92f7516
spasia3run <spasia3run@gmail.com>
2020-01-14 21:18:16 +0200

    add button up

--- js/main.js
+++ js/main.js
@@ -13,13 +13,12 @@ $(function(){
    }
   });
 
-  // add-open-class
-  $('.menu-item__link--dd-open').click(function(){
-   if($(this).parent().is('.menu-item--dd-opened')){
-    $(this).parent().removeClass('menu-item--dd-opened');
-   }else{
-    $(this).parent().addClass('menu-item--dd-opened');
-   }
+  //scroll up
+  $("a.scrollto").click(function () {
+    var elementClick = $(this).attr("href")
+    var destination = $(elementClick).offset().top;
+    jQuery("html:not(:animated),body:not(:animated)").animate({scrollTop: destination}, 800);
+    return false;
   });
 
 });
