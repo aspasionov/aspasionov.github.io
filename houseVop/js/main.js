16195fbbe1825ab09ab37477787e08b60393b2c7
spasia3run <spasia3run@gmail.com>
2020-01-15 22:19:19 +0200

    add housevop

---
+++ houseVop/js/main.js
@@ -0,0 +1,24 @@
+$(function(){
+  $('input[placeholder], textarea[placeholder]').placeholder();
+  $(".mask-phone").mask("+7 (999) 999-99-99");
+
+  // add-open-class
+  $('.burger-menu').click(function(){
+   if($(this).parent().is('.menu-burger--opened')){
+     $(this).parent().removeClass('menu-burger--opened');
+     $('body').removeClass('menu-open-wrapper-page');
+   }else{
+     $(this).parent().addClass('menu-burger--opened');
+     $('body').addClass('menu-open-wrapper-page');
+   }
+  });
+
+  //scroll up
+  $("a.scrollto").click(function () {
+    var elementClick = $(this).attr("href")
+    var destination = $(elementClick).offset().top;
+    jQuery("html:not(:animated),body:not(:animated)").animate({scrollTop: destination}, 800);
+    return false;
+  });
+
+});
