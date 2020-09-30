dd457efdc452730da01d2eae50a259b49cf650df
spasia3run <spasia3run@gmail.com>
2020-09-30 17:58:37 +0300

    add new page for remStanMash

--- remStanMash/js/main.js
+++ remStanMash/js/main.js
@@ -1,18 +1,40 @@
 $(function () {
 
 	// $(".mask-phone").mask("+7 (999) 999-99-99");
-
-	// add-open-class
-	$('.burger-menu').click(function () {
-		if ($(this).parent().is('.menu-burger--opened')) {
-			$(this).parent().removeClass('menu-burger--opened');
-			$('body').removeClass('menu-open-wrapper-page');
+	//mobile-menu-dropdown
+	// $('.catalog-mobile__link').on('click', function () {
+	// 	$(this).siblings('.catalog-mobile-submenu').slideToggle(300);
+	// })
+	// $('.catalog-mobile-submenu__link').on('click', function () {
+	// 	$(this).siblings('.catalog-mobile-submenu-dd').slideToggle(300);
+	// })
+	$('.catalog-mobile__link--open').on("click", function () {
+		$(this).parent().toggleClass('active')
+		if ($(this).parent().is(".active")) {
+			$(".catalog-mobile-submenu").slideDown(300)
 		} else {
-			$(this).parent().addClass('menu-burger--opened');
-			$('body').addClass('menu-open-wrapper-page');
+			$(".catalog-mobile-submenu").slideUp(300)
 		}
-	});
+	})
 
+	$('.catalog-mobile-submenu__link--open').on("click", function () {
+		$(this).parent().toggleClass('active')
+		if ($(this).parent().is(".active")) {
+			$(".catalog-mobile-submenu-dd").slideDown(300)
+		} else {
+			$(".catalog-mobile-submenu-dd").slideUp(300)
+		}
+	})
+	//menu
+	$('.burger-menu').on("click", function () {
+		$(this).parent().addClass('menu-burger--opened');
+		$('body').addClass('menu-open-wrapper-page');
+	})
+
+	$('.menu-close').on("click", function () {
+		$('.head-menu').removeClass('menu-burger--opened');
+		$('body').removeClass('menu-open-wrapper-page');
+	})
 	// dd menu
 	$('.header-line-menu__link').on("click", function () {
 		$(this).parent().toggleClass('active');
@@ -126,6 +148,44 @@ $(function () {
 			}
 		]
 	})
+	//news-slider 
+	$('.news-items--slider').slick({
+		slidesToShow: 1,
+		slidesToScroll: 1,
+		dots: true,
+		arrows: false,
+		rows: 0,
+		mobileFirst: true,
+		appendArrows: '.arrows-wrap',
+		nextArrow: `<div class="slide-arrow slide-prev">
+						<svg class="icon icon-next">
+						  <use xlink:href="img/icons/svg-symbols.svg#icon-next"></use>
+						</svg>
+					 </div>`,
+		prevArrow: `<div class="slide-arrow slide-next">
+						  <svg class="icon arrow-prev">
+							 <use xlink:href="img/icons/svg-symbols.svg#arrow-prev"></use>
+						  </svg>
+					</div>`,
+
+		responsive: [
+			{
+				breakpoint: 599,
+				settings: {
+					slidesToShow: 2
+				}
+			},
+			{
+				breakpoint: 999,
+				settings: {
+					slidesToShow: 3,
+					dots: false,
+					arrows: true
+				}
+			}
+		]
+	});
+
 	//slick-slider
 	$('.slider-items').slick({
 		slidesToShow: 1,
